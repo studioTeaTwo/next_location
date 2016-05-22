6ec8cf12a7963c4677f668fcff52ed9c957be59b
T2 <t2.tide@gmail.com>
2016-05-22 16:15:58 +0900

    Update V1__CREATE_INITIAL.sql

--- db/sql/V1__CREATE_INITIAL.sql
+++ db/sql/V1__CREATE_INITIAL.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS locations;
 create table locations(
   location_id integer primary key AUTO_INCREMENT,
   longitude varchar(255) not null,
@@ -7,7 +6,6 @@ create table locations(
   UNIQUE (longitude, latitude)
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS checkins;
 create table checkins(
     checkin_id bigint primary key AUTO_INCREMENT,
     user_id bigint not null,
@@ -16,7 +14,6 @@ create table checkins(
     updated_at TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS portals;
 create table portals(
   portal_id bigint primary key AUTO_INCREMENT,
   location_id VARCHAR(255) not null,
